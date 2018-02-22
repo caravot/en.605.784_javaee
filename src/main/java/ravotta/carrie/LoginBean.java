c643977fe688e7e93087d143cb35fcd16455ae91
Carrie Ravotta <carrie.peary@gmail.com>
2018-02-21 22:03:27 -0500

    Somethignn works...at least paths/libs do

---
+++ src/main/java/ravotta/carrie/LoginBean.java
@@ -0,0 +1,99 @@
+package ravotta.carrie;
+
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.security.Principal;
+
+public class LoginBean {
+    String username;
+    String uid;
+    String userLdapInfo;
+    HttpSession session;
+    StudentInfo userProfile;
+
+    private String[] attristr = { "uid", "userpassword", "authpassword", "authpassword", "authpassword" };
+
+    public LoginBean() {
+        super();
+    }
+
+    public LoginBean(HttpServletRequest request, HttpSession session) {
+        super();
+        this.session = session;
+        Principal princple = request.getUserPrincipal();
+        username = princple.getName();
+
+
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public String toString() {
+        return "This is the login bean";
+    }
+
+    public void doLogout() {
+        if (session != null) {
+            session.invalidate();
+        }
+        redirectToPage("/faces/welcome.jspx");
+    }
+
+    private void redirectToPage(String redirectUrl) {
+        final FacesContext fctx = FacesContext.getCurrentInstance();
+        try {
+            final ExternalContext ectx = fctx.getExternalContext();
+            ectx.redirect(ectx.getRequestContextPath() + redirectUrl);
+            fctx.responseComplete();
+        } catch (Exception e) {
+            fctx.responseComplete();
+        }
+    }
+
+    public void setUserLdapInfo(String userLdapInfo) {
+        this.userLdapInfo = userLdapInfo;
+    }
+
+    public String getUserLdapInfo() {
+        return userLdapInfo;
+    }
+
+//    public void setUserProfile(UserProfile userProfile) {
+//        this.userProfile = userProfile;
+//        try {
+//            for (Iterator<Property> itr = userProfile.getProperties(attristr).getAll(); itr.hasNext(); ) {
+//                Property prop = itr.next();
+//                userpropertyArray.add(new UserProperty(prop.getName(), prop.getValues()));
+//            }
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//
+//    public UserProfile getUserProfile() {
+//        return userProfile;
+//    }
+//
+//
+//    public ArrayList<UserProperty> getUserpropertyArray() {
+//        return userpropertyArray;
+//    }
+}
