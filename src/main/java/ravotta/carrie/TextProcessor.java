237466c9fa8fda1d13c1609aa07b7047153190c9
Carrie Ravotta <carrie.peary@gmail.com>
2018-04-09 16:34:21 -0400

    Removed unused files

--- src/main/java/ravotta/carrie/TextProcessor.java
+++
@@ -1,8 +0,0 @@
-package ravotta.carrie;
-
-import javax.ejb.Remote;
-
-@Remote
-public interface TextProcessor {
-    int length(String text);
-}
--- src/main/java/ravotta/carrie/TextProcessorLocal.java
+++
@@ -1,8 +0,0 @@
-package ravotta.carrie;
-
-import javax.ejb.Local;
-
-@Local
-public interface TextProcessorLocal {
-    int length(String text);
-}
